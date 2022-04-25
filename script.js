2a0c7cf15cda95f057de615c821a240f8fca56b6
aefjo <85477647+aefjo@users.noreply.github.com>
2022-04-25 14:39:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let imgbx = document.querySelector('#imgbx');
+let constructor = document.querySelector('.constructor');
+
+alert("Responsive Login & Registration Form , Click Signin / Signup to active Animation");
+
+function imgactive() {
+    imgbx.classList.toggle('active');
+}
+
+function constactive() {
+    constructor.classList.toggle('active');
+}
